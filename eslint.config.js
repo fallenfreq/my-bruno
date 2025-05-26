2161acd6e47a8c4678fa690e37fd68339856c810
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-05-26 18:20:44 +0100

    Update ESLint and Prettier configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,10 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
 import json from '@eslint/json';
 import markdown from '@eslint/markdown';
-import { defineConfig } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-// This is only required if extending a config with rules that conflict with prettier
-// import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
 	{
@@ -44,12 +41,10 @@ export default defineConfig([
 		plugins: {
 			'@stylistic': stylistic,
 		},
-		languageOptions: { parser: tsParser, sourceType: 'module', globals: globals.node },
+		languageOptions: { parser: tseslint.parser, sourceType: 'module', globals: globals.node },
 		ignores: ['**/dist'],
 		rules: {
 			'@stylistic/operator-linebreak': ['error', 'before'],
 		},
 	},
-	// This is only required if extending a config with rules that conflict with prettier
-	// eslintConfigPrettier
 ]);
